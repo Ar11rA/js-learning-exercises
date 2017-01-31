0019a0101dd279e3fa94976984d51aa0f1edcf30
Anuj Shankar <anujshankar95@gmail.com>
2017-01-31 18:59:36 +0530

    removed hello

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ function sum(firstNumber, secondNumber, callback){
 var sumOfNumbers = firstNumber + secondNumber;
 return callback(sumOfNumbers);
 }
-Hello!
 var changed = sum(5,6,function(ans){
   console.log(ans);
 });
