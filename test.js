4f1c167ce834c51d29eb1d857829c1b236aed5e7
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-01-31 18:54:57 +0530

    Kuch toh kiya

--- test.js
+++ test.js
@@ -2,7 +2,5 @@ function sum(firstNumber, secondNumber, callback){
 var sumOfNumbers = firstNumber + secondNumber;
 return callback(sumOfNumbers);
 }
-
-var changed = sum(5,6,function(ans){
-  console.log(ans);
+sadasd;
 });
