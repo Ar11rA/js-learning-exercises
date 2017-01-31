3af56819d3bb5ca74d3e865162275e99afe97c72
Anuj Shankar <anujshankar95@gmail.com>
2017-01-31 18:56:53 +0530

    hello changes

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ function sum(firstNumber, secondNumber, callback){
 var sumOfNumbers = firstNumber + secondNumber;
 return callback(sumOfNumbers);
 }
-
+Hello!
 var changed = sum(5,6,function(ans){
   console.log(ans);
 });
