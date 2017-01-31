850ccc9c987a50542b3d391cf9033e72bcb92698
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-01-31 19:41:39 +0530

    ho gya

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-function sum(firstNumber, secondNumber, callback){
-var sumOfNumbers = firstNumber + secondNumber;
-return callback(sumOfNumbers);
+function sum(firstNumber, secondNumber, callback) {
+
+  var sumOfNumbers = firstNumber + secondNumber;
+  setTimeout(function () {
+    return callback(sumOfNumbers);
+  }, 3000);
 }
+
 module.exports = sum;
