cc93c41308f786214434b7e91e88408200d26d6c
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-01-31 18:48:35 +0530

    Kuch toh kiya

---
+++ newsum.js
@@ -0,0 +1,2 @@
+var sumfn = require('./test');
+var x1 = sumfn(5,6,(ans)=>console.log(ans));
--- test.js
+++ test.js
@@ -2,4 +2,5 @@ function sum(firstNumber, secondNumber, callback){
 var sumOfNumbers = firstNumber + secondNumber;
 return callback(sumOfNumbers);
 }
-var x1 = sum(5,6,(ans)=>console.log(ans));
+//var x1 = sum(5,6,(ans)=>console.log(ans));
+module.exports = sum;
